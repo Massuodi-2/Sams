3908857297ad44c3b6caf8ebfb66886db2d9fd1f
Massuodi-2 <90631054+Massuodi-2@users.noreply.github.com>
2022-03-06 19:30:47 +0430

    network

--- network.js
+++
@@ -1,6 +0,0 @@
-function offline(){
-    document.getElementById("icon").innerHTML="  <link id='icon' rel='icon' href='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRDPeGSkowKXPXz8enFLV0JALF6AW28tH6F1g&usqp=CAU'>";
-}
-function online(){
-    document.getElementById("icon").innerHTML="  <link id='icon' rel='icon' href='logo.png'>";
-}
